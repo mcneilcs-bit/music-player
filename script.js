5d774238524003d511cccdb3580288c59c53082d
Cameryn McNeil <mcneilcs@vcu.edu>
2025-09-14 22:21:41 -0400

    script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const shuffleBtn = document.getElementById('shuffle');
 const repeatBtn = document.getElementById('repeat');
 // Song titles
 
-const songs = ['74', 'trouble', 'WeddingRing'];
+const songs = ['74', 'Trouble', 'WeddingRing'];
 let songIndex = 0;
 let isShuffle = false;
 let isRepeat = false;
